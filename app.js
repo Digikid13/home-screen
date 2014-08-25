31d3d3702d812a740f7e92a33400e29d18f136c7
Justin Cruz <digikid13@gmail.com>
2014-08-25 17:44:19 -0400

    finished basic sidebar actions

--- app.js
+++ app.js
@@ -1,23 +1,36 @@
 //create functions that make the api calls, show the selections of tiles when clicking on an empty div, bring up a side bar with all the tiles that may or may not move the page over
 var tileApp = {
   init: function() {
-    //$('#sidebar').hide();
+    // Give each empty tile a random background color
+    $(".empty").each(function() {
+      var randomColor = '' + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6);
+      $(this).css('background-color', randomColor);
+    });
+
+    // Add a function to each tile to show the sidebar
     $('.tile').on('click', function() {
       $('#sidebar').animate({'left': '0px'}, 100);
-      tileApp.update.apply(this);
+
+      $('#sidebar').attr('class', '');
+      $('#sidebar').addClass($(this).attr('id'));
+
+    });
+
+    $('#sidebar').on('click', 'li', function() {
+      tileApp.sidebar($('#sidebar').attr('class'), $(this).attr('id'));
     });
   },
-  update: function() {
-    $el = $(this);
+
+  // Either shows available tiles in the sidebar, (TODO) or options for the current tile
+  sidebar: function(tile, app) {
+    $el = $('#' + tile);
     if ($el.hasClass('empty')) {
-      var randomColor = '' + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6) + Math.round(Math.random()*6);
-      
       $el.removeClass('empty');
-      $el.addClass('something');
-      console.log(randomColor);
-      $el.css('background-color', randomColor);
+      $el.addClass(app);
     }
   },
+
+  // Basic ajax call wrapper
   ajaxCall: function (site, input, cb) {
     $.ajax({
         url: site,
